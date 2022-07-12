cbb57e9ef0e74101439228fe2716b5596561646e
Xenda <viper@Viper.local>
2022-07-12 18:42:39 +0600

    More STL Code

--- pair.cpp
+++ pair.cpp
@@ -3,6 +3,16 @@
 using namespace std;
 int main()
 {
+    pair<string, int> p;
+    pair<string, int> q;
+    p.first = "Sajib";
+    p.second = 21;
+    q = make_pair("Sabbir", 11);
+    pair<string, int> m("Sakib", 75);
+
+    cout << p.first << " " << p.second << endl;
+    cout << q.first << " " << q.second << endl;
+    cout << m.first << " " << m.second << endl;
 
     return 0;
 }
